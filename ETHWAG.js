f14e015e5acb88b271ba19467ea78b0f1dfa7ede
wyjson <wyjson@qq.com>
2022-03-15 16:22:25 +0800

    Update ETHWAG.js

--- ETHWAG.js
+++ ETHWAG.js
@@ -5,23 +5,23 @@
  * 离线生成以太坊钱包靓号地址(Generate an Ethereum wallet good account number offline)
  */
 //引入ethers.js
-var ethers = require('ethers');
+const ethers = require('ethers');
 console.log('ethers.version:'+ethers.version);
 
 //靓号正则
 // var a0x0 = new RegExp("^0x0","g");// 开头不能是0x0
 
-var aaaaaa = new RegExp("([\\d])\\1{5,}","g");// 111111或222222
-var dead0000 = new RegExp("^dead0000","g");// 以dead0000开头
+const aaaaaa = new RegExp("([\\d])\\1{5,}","g");// 111111或222222
+const dead0000 = new RegExp("^dead0000","g");// 以dead0000开头
 
-var AAAA1 = new RegExp("^([\\w])\\1{3,}","gi");// 前4位相同
-var AAAAA2 = new RegExp("([\\w])\\1{4,}$","gi");// 后5位相同
+const AAAA1 = new RegExp("^([\\w])\\1{3,}","gi");// 前4位相同
+const AAAAA2 = new RegExp("([\\w])\\1{4,}$","gi");// 后5位相同
 
-var a80808080 = new RegExp("^80808080","g");// 以80808080开头
-var a90909090 = new RegExp("^90909090","g");// 以90909090开头
-var a58585858 = new RegExp("^58585858","g");// 以58585858开头
+const a80808080 = new RegExp("^80808080","g");// 以80808080开头
+const a90909090 = new RegExp("^90909090","g");// 以90909090开头
+const a58585858 = new RegExp("^58585858","g");// 以58585858开头
 
-var AAAAAA1 = new RegExp("^([\\w])\\1{5,}","g");// 前6位相同
+const AAAAAA1 = new RegExp("^([\\w])\\1{5,}","g");// 前6位相同
 
 for(;;){
     //拿到生成的钱包信息
