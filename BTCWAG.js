4d3deaaf462151d49090030441fddfc38a319336
Wyjson <wyjson@qq.com>
2023-05-15 17:08:00 +0800

    Update BTCWAG.js

--- BTCWAG.js
+++ BTCWAG.js
@@ -5,12 +5,15 @@
  * 离线生成BTC钱包靓号地址(Generate an Bitcoin wallet good account number offline)
  * 1. npm install bip32@2.0.6
  * 2. npm install bip39@3.0.4
- * 3. npm install bitcoinjs-lib@6.0.1
+ * 3. npm install bitcoinjs-lib
+ * 4. npm install tiny-secp256k1
  */
 const bip39 = require('bip39');
 const bip32 = require('bip32');
 const bitcoin = require('bitcoinjs-lib');
 const network = bitcoin.networks.bitcoin;
+const ecc = require('tiny-secp256k1');
+bitcoin.initEccLib(ecc);
 
 //靓号正则
 const w4 = new RegExp("^([\\w])\\1{3,}","g");// 前4位相同
@@ -32,39 +35,41 @@ for(;;){
     var address = p2pkh.address;
 
     // Nested SegWit (P2SH)格式 3LoE438ufVn99qgaX5Gwvi22iAgKBzF9Hd
-    var swPath = bip32.fromSeed(seed).derivePath("m/49'/0'/0'/0/0");// sw 隔离见证
+    var swPath = bip32.fromSeed(seed).derivePath("m/49'/0'/0'/0/0");
     var privateKey3 = swPath.toWIF();
     var p2wpkh = bitcoin.payments.p2sh({ redeem: bitcoin.payments.p2wpkh({ pubkey: swPath.publicKey, network : network }) })
     var address3 = p2wpkh.address;
 
     // Native SegWit (Bech32)格式 bc1qjmfmsaj2mvgd7n46qrms6avlu0ku4qgrns9x7w
-    var bech32Path = bip32.fromSeed(seed).derivePath("m/84'/0'/0'/0/0");// sw 隔离见证
+    var bech32Path = bip32.fromSeed(seed).derivePath("m/84'/0'/0'/0/0");
     var privateKey3bc1q = bech32Path.toWIF();   
     var bc1q = bitcoin.payments.p2wpkh({ pubkey: bech32Path.publicKey, network : network });
     var address3bc1q = bc1q.address;
 
     // Taproot (P2TR)格式 bc1ptyurxq9h7v4p6va48pd4nhu5lup9ttszhpnn3fggg59dup6j9n9srk3030
-    // ...
-
+    var bech32mPath = bip32.fromSeed(seed).derivePath("m/86'/0'/0'/0/0");
+    var privateKey3bc1p = bech32mPath.toWIF();   
+    var bc1p = bitcoin.payments.p2tr({ internalPubkey: bech32mPath.publicKey.slice(1, 33), network : network });
+    var address3bc1p = bc1p.address;
 
     var isLog = false;
 
-    if (w4.exec(address3.substring(1)) != null) {
+    if (w4.exec(address3bc1p.substring(4)) != null) {
         isLog = true;
         console.log("正则(^w4)")
     }
 
-    if (aabb.exec(address3.substring(1)) != null) {
+    if (aabb.exec(address3bc1p.substring(4)) != null) {
         isLog = true;
         console.log("正则(^AABB)")
     }
 
-    if (abab.exec(address3.substring(1)) != null) {
+    if (abab.exec(address3bc1p.substring(4)) != null) {
         isLog = true;
         console.log("正则(^ABAB)")
     }
 
-    if (BTC.exec(address3.substring(1)) != null) {
+    if (BTC.exec(address3bc1p.substring(4)) != null) {
         isLog = true;
         console.log("正则(^BTC)")
     }
@@ -76,6 +81,8 @@ for(;;){
         console.log(`私钥Nested SegWit (P2SH)： ${privateKey3}`);
         console.log(`地址Native SegWit (Bech32)： ${address3bc1q}`);
         console.log(`私钥Native SegWit (Bech32)： ${privateKey3bc1q}`);
+        console.log(`地址Taproot (P2TR)： ${address3bc1p}`);
+        console.log(`私钥Taproot (P2TR)： ${privateKey3bc1p}`);
         console.log(`助记词： ${mnemonic}`)
         console.log("-------------------------------------------------------------------")
     }
